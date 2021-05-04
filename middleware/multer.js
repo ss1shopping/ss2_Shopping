1153180d67e2d5c3a3f800d26103eb78861c0c2a
Manh Tien <nguyenmtien465@gmail.com>
2021-05-04 08:19:11 +0700

    pull category

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,23 +1,23 @@
-let multer =require('multer')
-const storage=multer.diskStorage({
-  destination:function(req,file,cb){
-    cb(null,'public/images')
+let multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images')
   },
-  filename: function(req,file,cb){
-    cb(null ,Date.now() + file.originalname) 
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname)
   }
 })
 const fileFilter = (req, file, cb) => {
-    // reject a file
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' ||file.mimetype === 'image/jpg' || file.mimetype==="text/plain") {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  };
- const upload=multer({
-  storage:storage,
-  limits:{fileSize:1000000000000},
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000000000 },
   fileFilter: fileFilter
 })
 
